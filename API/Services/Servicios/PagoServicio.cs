566e8e13f8e9025507878e0b01a4ac49369e101d
Luna Hazuki <LunaHazuki2006@outlook.com>
2024-04-10 17:39:30 -0400

    Con todo el funcionamiento de prestamos servicios

--- API/Services/Servicios/PagoServicio.cs
+++ API/Services/Servicios/PagoServicio.cs
@@ -1,4 +1,5 @@
 using Core.Entidades;
+using Core.Interfaces;
 using Core.Interfaces.Servicios;
 using Core.Respuestas;
 
@@ -6,6 +7,11 @@ namespace Services.Servicios
 {
     public class PagoServicio : IPagoServicio
     {
+        private readonly IUnidadDeTrabajo _unidadDeTrabajo;
+		public PagoServicio(IUnidadDeTrabajo unidadDeTrabajo)
+		{
+			_unidadDeTrabajo = unidadDeTrabajo;
+		}
         public Task<Respuesta<Pago>> Actualizar(int entidadParaActualizarId, Pago nuevosValoresEntidad)
         {
             throw new NotImplementedException();
@@ -31,15 +37,29 @@ namespace Services.Servicios
             throw new NotImplementedException();
         }
 
-        public Task<Respuesta<Pago>> RealizarPago(Pago pago)
+        public async Task<Respuesta<Pago>> RealizarPago(Pago pago)
         {
             throw new NotImplementedException();
         }
 
         // Pensar bien como hacer esta
-        public Task<Respuesta<IEnumerable<Pago>>> ConsultarPagosPorPrestamo(int IdPrestamo)
+        public async Task<Respuesta<IEnumerable<Pago>>> ConsultarPagosPorPrestamo(int IdPrestamo)
         {
-            throw new NotImplementedException();
+            var prestamo = await _unidadDeTrabajo.PrestamoRepostorio.ObtenerPorIdAsincrono(IdPrestamo);
+            if (prestamo == null) throw new ArgumentException("No existe un préstamo con tal id");
+            var todos = await _unidadDeTrabajo.CuotaRepositorio.ObtenerTodosAsincrono();
+            var listaCuotas = todos.ToList().FindAll(x => x.IdPrestamo == IdPrestamo);
+            var listaPagos = new List<Pago>();
+            foreach (var cuota in listaCuotas) {
+                var pago = await _unidadDeTrabajo.PagoRepositorio.ObtenerPorIdAsincrono(cuota.Id);
+                listaPagos.Add(pago);
+            }
+            var respuesta = new Respuesta<IEnumerable<Pago>>() {
+                Datos = listaPagos, 
+                Mensaje = "Obtención de préstamos exitosa", 
+                Ok = true
+            };
+            return respuesta;
         }
 
     }
--- API/Services/Servicios/PrestamoServicio.cs
+++ API/Services/Servicios/PrestamoServicio.cs
@@ -1,4 +1,5 @@
 using Core.Entidades;
+using Core.Interfaces;
 using Core.Interfaces.Servicios;
 using Core.Respuestas;
 
@@ -6,6 +7,11 @@ namespace Services.Servicios
 {
     public class PrestamoServicio : IPrestamoServicio
     {
+        private readonly IUnidadDeTrabajo _unidadDeTrabajo;
+		public PrestamoServicio(IUnidadDeTrabajo unidadDeTrabajo)
+		{
+			_unidadDeTrabajo = unidadDeTrabajo;
+		}
         public Task<Respuesta<Prestamo>> Actualizar(int entidadParaActualizarId, Prestamo nuevosValoresEntidad)
         {
             throw new NotImplementedException();
@@ -31,19 +37,70 @@ namespace Services.Servicios
             throw new NotImplementedException();
         }
 
-        public Task<Respuesta<Documento>> GuardarDocumento(Documento documento) 
+        public async Task<Respuesta<Documento>> GuardarDocumento(Documento documento) 
         {
-            var respuesta = new Respuesta<Documento>();
-            throw new NotImplementedException();
+            try
+            {
+                if (documento == null) throw new ArgumentException("El documento está vacío");
+                var prestamo = await _unidadDeTrabajo.PrestamoRepostorio.ObtenerPorIdAsincrono(documento.IdPrestamo);
+                if (prestamo == null) throw new ArgumentException("El préstamo de este documento no existe");
+                var tipo = await _unidadDeTrabajo.TipoDocumentoRepositorio.ObtenerPorIdAsincrono(documento.IdTipo);
+                if (tipo == null) throw new ArgumentException("El tipo de este documento no existe");
+                await _unidadDeTrabajo.DocumentoRepositorio.AgregarAsincrono(documento);
+                var respuesta = new Respuesta<Documento>() {
+                    Datos = documento, 
+                    Ok = true, 
+                    Mensaje = "Documento ha sido guardado correctamente"
+                };
+                return respuesta;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            
         }
 
-        public Task<Respuesta<IEnumerable<Prestamo>>> ConsultarPrestamosDeCliente(int IdCliente) {
-            throw new NotImplementedException();
+        public async Task<Respuesta<IEnumerable<Prestamo>>> ConsultarPrestamosDeCliente(int IdCliente) 
+        {    
+            try
+            {
+                var cliente = await _unidadDeTrabajo.ClienteRepositorio.ObtenerPorIdAsincrono(IdCliente);
+                if (cliente == null) throw new ArgumentException("No existe cliente con tal id");
+                var todos = await _unidadDeTrabajo.PrestamoRepostorio.ObtenerTodosAsincrono();
+                var lista = todos.ToList().FindAll(x => x.IdCliente == IdCliente);
+                var respuesta = new Respuesta<IEnumerable<Prestamo>>() {
+                    Datos = lista, 
+                    Mensaje = "Prestamos encontrados exitósamente", 
+                    Ok = true
+                };
+                return respuesta;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
         }
 
-        public Task<Respuesta<IEnumerable<Documento>>> ConsultarDocumentos(int IdPrestamo)
+        public async Task<Respuesta<IEnumerable<Documento>>> ConsultarDocumentos(int IdPrestamo)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var prestamo = await _unidadDeTrabajo.DocumentoRepositorio.ObtenerPorIdAsincrono(IdPrestamo);
+                if (prestamo == null) throw new ArgumentException("No existe un préstamo con tal id");
+                var todos = await _unidadDeTrabajo.DocumentoRepositorio.ObtenerTodosAsincrono();
+                var lista = todos.ToList().FindAll(x => x.IdPrestamo == IdPrestamo);
+                var respuesta = new Respuesta<IEnumerable<Documento>>() {
+                    Datos = lista, 
+                    Mensaje = "Los documentos fueron encontrados exitósamente", 
+                    Ok = true
+                };
+                return respuesta;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
         }
     }
 }
