67b02fc3706982c4872813d25f861ced1dca7ad4
Luna Hazuki <LunaHazuki2006@outlook.com>
2024-04-11 06:23:17 -0400

    Commit precautivo

--- API/Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
+++ API/Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88cc8f8b79fb59ed2694211f91ea990a815413ad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+566e8e13f8e9025507878e0b01a4ac49369e101d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- API/Services/obj/Debug/net8.0/Services.AssemblyInfo.cs
+++ API/Services/obj/Debug/net8.0/Services.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88cc8f8b79fb59ed2694211f91ea990a815413ad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+566e8e13f8e9025507878e0b01a4ac49369e101d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
