78a09fb0ca1ed5a0138bb40744013d7f0b7a6b38
Siberiaaa <siberiamvg@gmail.com>
2024-04-15 09:26:55 -0400

    a la clase auxiliar para realizar registros de usuario se le cambió la fecha a dateonly

--- API/Core/Respuestas/ModeloRegistrarse.cs
+++ API/Core/Respuestas/ModeloRegistrarse.cs
@@ -8,7 +8,7 @@
 		public string Apellido { get; set; }
 		public string Cedula { get; set; }
 		public string Correo { get; set; }
-		public string FechaNacimiento { get; set; }
+		public DateOnly FechaNacimiento { get; set; }
 		public string Telefono { get; set; }
 		public string Direccion { get; set; }
 	}
