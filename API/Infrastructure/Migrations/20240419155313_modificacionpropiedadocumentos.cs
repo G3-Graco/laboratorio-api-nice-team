a0a8290a2b4506715f063935025af1089cd9c093
Siberiaaa <siberiamvg@gmail.com>
2024-04-19 11:54:14 -0400

    migracion de la modificiacion de propiedad a la entidad documento

---
+++ API/Infrastructure/Migrations/20240419155313_modificacionpropiedadocumentos.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class modificacionpropiedadocumentos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "documento",
+                table: "Documentos",
+                type: "bytea",
+                nullable: true,
+                oldClrType: typeof(byte[]),
+                oldType: "bytea");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Ubicacion",
+                table: "Documentos",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Ubicacion",
+                table: "Documentos");
+
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "documento",
+                table: "Documentos",
+                type: "bytea",
+                nullable: false,
+                defaultValue: new byte[0],
+                oldClrType: typeof(byte[]),
+                oldType: "bytea",
+                oldNullable: true);
+        }
+    }
+}
